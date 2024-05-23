364f1f459c58300e74a02284c62bdb098e6737d5
MetRyan <101856114+MetRyan@users.noreply.github.com>
2024-05-23 11:49:58 +0700

    Db first

---
+++ Lab_01_AutomobileManagement_Using_EntityFramework and WPF/AutomobileSolution/AutomobileLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace AutomobileLibrary
+{
+    public class Class1
+    {
+
+    }
+}
+//dotnet ef dbcontext scaffold "server =(local); database = MyShock;uid=sa;pwd=sa;" Microsoft.EntityFrameworkCore.SqlServer --output-dir DataAccess
+//dotnet ef dbcontext scaffold "server =(local); database = MyShock;uid=sa;pwd=sa;" Microsoft.EntityFrameworkCore.SqlServer --output-dir DataAccess
---
+++ Lab_01_AutomobileManagement_Using_EntityFramework and WPF/AutomobileSolution/AutomobileLibrary/DataAccess/CarManagement.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileLibrary.DataAccess
+{
+    public class CarManagement
+    {
+        //-- Using Singleton Pattern
+        private static CarManagement instance = null;
+        public static readonly object instanceLock = new object();
+        private CarManagement() { }
+
+        public static CarManagement Instance
+        {
+
+            get
+            {
+                    lock (instanceLock)
+                    {
+                        if (instance == null)
+                        {
+
+                            instance = new CarManagement();
+                        }
+                        return instance;
+
+                    }
+                }
+
+            }
+        }
+    }
